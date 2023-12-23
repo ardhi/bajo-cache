5d4a2da1c0201109a065ccb7948fbfcc69bc17e4
Ardhi Lukianto <ardhi@lukianto.com>
2023-12-23 21:40:52 +0700

    Updates

--- bajo/helper/clear-coll.js
+++ bajo/helper/clear-coll.js
@@ -1,15 +1,23 @@
-async function clearColl (name) {
+async function clearColl ({ coll, id, body, record, options } = {}) {
   const { getConfig } = this.bajo.helper
   const { getInfo, recordFind, recordRemove } = this.bajoDb.helper
   const cfg = getConfig('bajoCache')
-  const { connection } = await getInfo(name)
-  if (connection.memory) return false
-  if (cfg.collection.disabled.includes(name)) return
-  const query = { coll: name }
-  const recs = await recordFind('CacheStorage', { query, limit: 1000 }, { skipHook: true, skipCache: true })
-  for (const r of recs) {
-    await recordRemove('CacheStorage', r.id, { skipHook: true })
-  }
+  const clear = cfg.collection.clearOnTrigger[coll] ?? cfg.collection.defClearOnTrigger
+  if (!clear) return
+  try {
+    const { connection } = await getInfo(coll)
+    if (connection.memory) return false
+    if (cfg.collection.disabled.includes(coll)) return
+    /*
+    let action = 'create'
+    if (id) action = body ? 'update' : 'remove'
+    */
+    const query = { coll }
+    const recs = await recordFind('CacheStorage', { query, limit: 1000 }, { skipHook: true, skipCache: true })
+    for (const r of recs) {
+      await recordRemove('CacheStorage', r.id, { skipHook: true })
+    }
+  } catch (err) {}
 }
 
 export default clearColl

91d1aabd060cdedff03fcadf9c99b8cddf77f225
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-23 23:31:47 +0700

    Updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import removeExpired from './lib/remove-expired.js'
 async function factory (pkgName) {
   const me = this
 
-  return class BajoCache extends this.lib.BajoPlugin {
+  return class BajoCache extends this.lib.Plugin {
     constructor () {
       super(pkgName, me.app)
       this.alias = 'cache'
